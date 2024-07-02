b968549f4341235d37b3bfabcc1f34d5cf650cb1
Bragabriel <gabriel.bragasilva@hotmail.com>
2024-07-02 19:40:12 -0300

    fix

--- easy/TwoSum.java
+++ easy/TwoSum.java
@@ -12,8 +12,8 @@ public class TwoSum {
 
         Scanner scanner = new Scanner(System.in);
 
-        int[] nums = { 3, 2, 4 };
-        int target = 6;
+        int[] nums = { 2, 5, 5, 11 };
+        int target = 10;
 
         int[] retorno = twoSum(nums, target);
         System.out.println(Arrays.toString(retorno));
@@ -25,7 +25,7 @@ public class TwoSum {
 
         for (Integer i = 0; i < nums.length; i++) {
             for (Integer k = 1; k < nums.length; k++) {
-                if ((nums[i] + nums[k]) == target) {
+                if ((nums[i] + nums[k]) == target && (i != k)) {
                     return new int[] { i, k };
                 }
             }
