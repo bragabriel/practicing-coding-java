e23b9accca8aa97c035da5408de81f7b634418cf
Bragabriel <gabriel.bragasilva@hotmail.com>
2024-08-06 15:42:26 -0300

    add files

--- medium/LED_v2.java
+++ medium/LED_v2.java
@@ -1,11 +1,12 @@
 package medium;
 
 import java.io.IOException;
+import java.util.HashMap;
 import java.util.Scanner;
 
-public class LED {
+public class LED_v2 {
     /**
-     * João quer montar um painel de leds contendo diversos números. Ele não possui muitos leds, e não tem 
+     * João quer montar um painel de leds contendo diversos números. Ele não possui muitos leds, e não tem
      * certeza se conseguirá montar o número desejado. Considerando a configuração dos leds dos números
      * abaixo, faça um algoritmo que ajude João a descobrir a quantidade de leds necessário para montar o
      * valor.
@@ -29,25 +30,22 @@ public class LED {
     public static Integer matchesAndCount(String num){
         Integer numOfLed = 0;
 
-        for(Integer i=0; i<num.length(); i++){
-            char letter = num.charAt(i);
-            if(letter == '1'){
-                numOfLed += 2;
-            }
-            else if(letter == '7'){
-                numOfLed += 3;
-            }
-            else if(letter == '4'){
-                numOfLed += 4;
-            }
-            else if(letter == '2' || letter == '3' | letter == '5'){
-                numOfLed += 5;
-            }
-            else if(letter == '6' || letter == '9' || letter == '0'){
-                numOfLed +=6;
-            }
-            else if(letter == '8'){
-                numOfLed += 7;
+        HashMap<Character, Integer> ledMap = new HashMap<>();
+
+        ledMap.put('1', 2);
+        ledMap.put('7', 3);
+        ledMap.put('4', 4);
+        ledMap.put('2', 5);
+        ledMap.put('3', 5);
+        ledMap.put('5', 5);
+        ledMap.put('6', 6);
+        ledMap.put('9', 6);
+        ledMap.put('0', 6);
+        ledMap.put('8', 7);
+
+        for(char letter : num.toCharArray()){
+            if (ledMap.containsKey(letter)) {
+                numOfLed += ledMap.get(letter);
             }
         }
 
